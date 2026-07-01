52e636588277e0b22b58cf4aae79a6c7443a3863
SzymCode <szymon.radomski@yahoo.com>
2026-07-01 15:47:39 +0700

    fix: correct nui-heading & nui-paragraph styles

--- src/components/nui-heading/nui-heading.ts
+++ src/components/nui-heading/nui-heading.ts
@@ -1,37 +1,16 @@
 import { LitElement, type PropertyValues } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { type NuiType, nuiTypeProperty } from '../../types/nui-type.js';
-import { syncStylesWhen } from '../../utils/sync-stylesheet.js';
+import {
+  createComponentStyles,
+  syncStylesWhen,
+} from '../../utils/sync-stylesheet.js';
 import { type NuiHeadingViewState, renderHeading } from './logic.js';
 
-let baseStylesheet: CSSStyleSheet | null = null;
-let themeStylesheet: CSSStyleSheet | null = null;
-
-async function syncHeadingStyles(
-  renderRoot: Element | DocumentFragment,
-  unstyled: boolean,
-): Promise<void> {
-  if (!(renderRoot instanceof ShadowRoot)) {
-    return;
-  }
-
-  if (!baseStylesheet) {
-    const baseModule = await import('./base.css', { with: { type: 'css' } });
-    baseStylesheet = baseModule.default;
-  }
-
-  if (unstyled) {
-    renderRoot.adoptedStyleSheets = [baseStylesheet];
-    return;
-  }
-
-  if (!themeStylesheet) {
-    const themeModule = await import('./styles.css', { with: { type: 'css' } });
-    themeStylesheet = themeModule.default;
-  }
-
-  renderRoot.adoptedStyleSheets = [baseStylesheet, themeStylesheet];
-}
+const styles = createComponentStyles(
+  'nui-heading',
+  () => import('./styles.css', { with: { type: 'css' } }),
+);
 
 @customElement('nui-heading')
 export class NuiHeading extends LitElement implements NuiHeadingViewState {
@@ -42,12 +21,12 @@ export class NuiHeading extends LitElement implements NuiHeadingViewState {
   @property({ type: String, attribute: 'heading-class' }) headingClass = '';
 
   protected firstUpdated() {
-    void syncHeadingStyles(this.renderRoot, this.unstyled);
+    void styles.sync(this.renderRoot, { unstyled: this.unstyled });
   }
 
   protected updated(changed: PropertyValues) {
     syncStylesWhen(changed, 'unstyled', () =>
-      syncHeadingStyles(this.renderRoot, this.unstyled),
+      styles.sync(this.renderRoot, { unstyled: this.unstyled }),
     );
   }
 
--- src/components/nui-paragraph/nui-paragraph.ts
+++ src/components/nui-paragraph/nui-paragraph.ts
@@ -1,37 +1,16 @@
 import { LitElement, type PropertyValues } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { type NuiType, nuiTypeProperty } from '../../types/nui-type.js';
-import { syncStylesWhen } from '../../utils/sync-stylesheet.js';
+import {
+  createComponentStyles,
+  syncStylesWhen,
+} from '../../utils/sync-stylesheet.js';
 import { type NuiParagraphViewState, renderParagraph } from './logic.js';
 
-let baseStylesheet: CSSStyleSheet | null = null;
-let themeStylesheet: CSSStyleSheet | null = null;
-
-async function syncParagraphStyles(
-  renderRoot: Element | DocumentFragment,
-  unstyled: boolean,
-): Promise<void> {
-  if (!(renderRoot instanceof ShadowRoot)) {
-    return;
-  }
-
-  if (!baseStylesheet) {
-    const baseModule = await import('./base.css', { with: { type: 'css' } });
-    baseStylesheet = baseModule.default;
-  }
-
-  if (unstyled) {
-    renderRoot.adoptedStyleSheets = [baseStylesheet];
-    return;
-  }
-
-  if (!themeStylesheet) {
-    const themeModule = await import('./styles.css', { with: { type: 'css' } });
-    themeStylesheet = themeModule.default;
-  }
-
-  renderRoot.adoptedStyleSheets = [baseStylesheet, themeStylesheet];
-}
+const styles = createComponentStyles(
+  'nui-paragraph',
+  () => import('./styles.css', { with: { type: 'css' } }),
+);
 
 @customElement('nui-paragraph')
 export class NuiParagraph extends LitElement implements NuiParagraphViewState {
@@ -42,12 +21,12 @@ export class NuiParagraph extends LitElement implements NuiParagraphViewState {
   paragraphClass = '';
 
   protected firstUpdated() {
-    void syncParagraphStyles(this.renderRoot, this.unstyled);
+    void styles.sync(this.renderRoot, { unstyled: this.unstyled });
   }
 
   protected updated(changed: PropertyValues) {
     syncStylesWhen(changed, 'unstyled', () =>
-      syncParagraphStyles(this.renderRoot, this.unstyled),
+      styles.sync(this.renderRoot, { unstyled: this.unstyled }),
     );
   }
 
